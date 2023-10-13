aef50722529e05f4c2cd52efd36c535d97c3d83f
Bevis <>
2023-10-13 16:34:40 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,23 @@
 <script setup lang="ts">
+import { useTimeout } from '@vueuse/core'
+import anime from 'animejs'
 import { RouterView } from 'vue-router'
+
+// loading anime
+const ready = useTimeout(2000)
+onMounted(() => {
+  anime({
+    targets: '.loading',
+    duration: 2000,
+    easing: 'linear',
+    width: '100%'
+  })
+})
 </script>
 
 <template>
-  <RouterView />
+  <RouterView v-if="ready" />
+  <div v-else class="loading w0 h4 bg-emerald hover:cursor-none"></div>
   <CParticle />
 </template>
 
